22b0d25fd3995fbf4af0fb1d1dbd6fbf8255cdaa
testscreentool-lgtm <testscreentool@gmail.com>
2026-01-11 10:54:49 +0300

    Update Analytics.tsx

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -1,25 +1,5 @@
 'use client'
 
-import { useEffect } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
-
-declare global {
-  interface Window {
-    gtag: any
-  }
-}
-
 export default function Analytics() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.gtag) {
-      window.gtag('config', 'G-9WVWNB3SQP', {
-        page_path: pathname + (searchParams ? searchParams.toString() : ''),
-      })
-    }
-  }, [pathname, searchParams])
-
   return null
 }

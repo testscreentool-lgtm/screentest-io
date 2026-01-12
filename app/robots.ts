d1d7b80001f24dbc3aae07a18a686f9e5e1aa5dd
testscreentool-lgtm <testscreentool@gmail.com>
2026-01-12 14:52:09 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,13 +2,11 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/private/'],
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
     sitemap: 'https://screentest.io/sitemap.xml',
   }
 }

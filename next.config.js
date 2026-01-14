319a68703dcc920bf609cd21ae31f0f73f61b509
testscreentool-lgtm <testscreentool@gmail.com>
2026-01-14 21:07:59 +0300

    Update next.config.js
    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: '/black-screen-test',
+        destination: '/black-screen',
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
